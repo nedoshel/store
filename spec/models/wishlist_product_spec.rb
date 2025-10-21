e1660ec358c3f9e3d7698166a5cccc67027ab03a
nedoshel <nedoshel@gmail.com>
2025-10-21 03:19:49 +0300

    some specs

--- spec/models/wishlist_product_spec.rb
+++ spec/models/wishlist_product_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe WishlistProduct, type: :model do
   describe "associations" do
-    it { should belong_to(:product).counter_cache(:wishlists_count) }
+    it { should belong_to(:product).counter_cache(:wishlists_count).touch }
     it { should belong_to(:wishlist).counter_cache(:products_count) }
   end
 
@@ -14,5 +14,22 @@ RSpec.describe WishlistProduct, type: :model do
     subject { described_class.new(product: product, wishlist: wishlist) }
 
     it { should validate_uniqueness_of(:product_id).scoped_to(:wishlist_id) }
+  end
+
+  describe "scopes" do
+    let(:user) { create(:user) }
+    let(:product) { create(:product) }
+    let(:wishlist1) { Wishlist.create!(name: "w1", user: user) }
+    let(:wishlist2) { Wishlist.create!(name: "w2", user: user) }
+
+    before do
+      WishlistProduct.create!(product: product, wishlist: wishlist2)
+    end
+
+    it "returns records for the given wishlist via .by_wishlist" do
+      wishlist_product1 = WishlistProduct.create!(product: product, wishlist: wishlist1)
+
+      expect(WishlistProduct.by_wishlist(wishlist1)).to contain_exactly(wishlist_product1)
+    end
   end
 end
